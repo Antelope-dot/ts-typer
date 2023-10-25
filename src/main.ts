33ceed4da8d76a75d3b209a8daf6927098adcd3c
Antelope-dot <19973844+Antelope-dot@users.noreply.github.com>
2023-10-25 22:35:00 +0300

    Added text that floats from right to left

--- src/main.ts
+++ src/main.ts
@@ -6,23 +6,41 @@ canvas.height = 576;
 
 c.fillRect(0,0, canvas.width, canvas.height);
 
+const word_speed: number = 0.2;
+
 class Word {
   position: any;
+  word: string;
 
-  constructor(position: any) {
+  constructor(position: any, word: string) {
     this.position = position;
+    this.word = word;
   }
 
-  draw() {
-    c.fillStyle = 'white'
+  draw(): void {
+    c.fillStyle = 'white';
     c.font = "24px serif";
-    c.fillText("UwU", 50, 30)
+    c.fillText(this.word, this.position.x, this.position.y)
+  }
+
+  update(): void {
+    this.draw();
+    this.position.x += word_speed;
   }
 }
 
-const word = new Word({
-  x: 0,
-  y:0
-})
+const word = new Word(
+  {x: 0, y: 100}, 
+  "Test"
+)
 
-word.draw();
+//Game loop
+function animate(): void {
+  window.requestAnimationFrame(animate)
+  // Clear the canvas so drawings dont smear
+  c.fillStyle='black'
+  c.fillRect(0,0, canvas.width, canvas.height)
+  word.update();
+}
+
+animate();
